a95d455e4b203e9c38d915f21bd0d21283607468
Adrian <admd911@hotmail.com>
2018-09-14 14:15:29 +0100

    IteratePages.py now working.
    Signed-off-by: Adrian <admd911@hotmail.com>

--- IteratePages.py
+++ IteratePages.py
@@ -3,9 +3,37 @@
 from OpenDocx import get_text
 
 
-def turn_pages(file, startpage=499, endpage=712):
+def find_page(file, findpage):
+    """Iterates through document from page 499-712 and finds the beginning point of a desired page.
+       Returns position."""
+    stringplace = 0
+    if findpage < 499:
+        findpage = 499
+    if findpage > 712:
+        findpage = 499
+    if findpage == 499:
+        return stringplace
+    else:
+        workstring = file
+        curpage = 499
+        for i in range(499, findpage + 1):
+            if curpage < findpage:
+                curplace = workstring.find(str(curpage))
+                stringplace += curplace
+                workstring = workstring[curplace:]
+                curpage += 1
+            elif curpage == findpage:
+                tempstring = workstring
+                tempplace = tempstring.find(str(curpage))
+                tempstring = tempstring[:tempplace]
+                tempstring = tempstring[:tempstring.rfind("\n")]
+                curplace = len(tempstring) + 1
+                stringplace += curplace
+                workstring = workstring[curplace:]
+        return stringplace
 
-    print(file)
 
-
-turn_pages(get_text("Wurzburg Glosses"))
+# glosses = get_text("Wurzburg Glosses")
+# pagepoint = find_page(glosses, 509)
+# print(pagepoint)
+# print(glosses[pagepoint:])
