ad23692d8d5e61c2c71c651ae0c8c19899394ef1
Adrian <admd911@hotmail.com>
2018-09-14 20:02:40 +0100

    New file, GetSections.py included to isolate sections of text.
    Signed-off-by: Adrian <admd911@hotmail.com>

--- GetSections.py
+++ GetSections.py
@@ -1,12 +1,46 @@
-"""Level 1."""
+"""Level 2."""
 
+from GetTagText import get_tagtext
 from OpenPages import get_pages
 
 
 def get_section(fulltext, section="No Section"):
-    """Gets and returns just the text between a particular set of section tags"""
-    return fulltext
+    """Gets and returns list of instances of just the text between a particular set of section tags"""
+    if section != "Lat":
+        if section != "SG":
+            if section != "Eng":
+                if section != "FN":
+                    section = "No Section"
+    if section != "No Section":
+        secs = get_tagtext(fulltext, section)
+    else:
+        secremovetext = fulltext
+        seclist = ["Lat", "SG", "Eng", "FN"]
+        for sec in seclist:
+            opentag = ("[" + sec + "]")
+            closetag = ("[/" + sec + "]")
+            if opentag in secremovetext:
+                remcount = secremovetext.count(opentag)
+                for i in range(remcount):
+                    secremovetext = secremovetext[:secremovetext.find(opentag)] + "***SPLITHERE***" + \
+                                    secremovetext[secremovetext.find(closetag) + len(closetag):]
+        secs = secremovetext.split("***SPLITHERE***")
+        newsects = []
+        for sect in secs:
+            sect = sect.strip()
+            newsects.append(sect)
+        secs = newsects
+        spaces = "\n\n\n\n"
+        if spaces in "\n".join(secs):
+            secs = "\n".join(secs)
+            secs = secs.split(spaces)
+        spaces = "\n\n\n"
+        if spaces in "\n".join(secs):
+            secs = "\n".join(secs)
+            secs = secs.split(spaces)
+    return secs
 
 
-glosses = get_pages("Wurzburg Glosses", 499, 499)
-print(get_section(glosses))
+# glosses = get_pages("Wurzburg Glosses", 499, 712)
+# print("\n\n".join(get_section(glosses, "SG")))
+# print("\n".join(get_section(glosses)))
