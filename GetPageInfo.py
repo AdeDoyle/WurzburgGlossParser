cd55e7357db1aea80a5a3cdec3ed8f702ae4accc
Adrian <admd911@hotmail.com>
2018-09-18 18:24:51 +0100

    GetPageInfo.py added to create a dictionary containing all page information.
    Signed-off-by: Adrian <admd911@hotmail.com>

---
+++ GetPageInfo.py
@@ -0,0 +1,11 @@
+"""Level High"""
+# Go page by page and get page text.
+# For every page, get pageno, any headings, sections, and any footnotes in dict.
+#     Remove the heading, section and footnote tags.
+
+
+def get_pageinfo(pages):
+    return(pages)
+
+
+print(get_pageinfo("hi"))
