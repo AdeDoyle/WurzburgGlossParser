86b3f077c06a2ee1d203ac35fc313f6256759aca
Adrian <admd911@hotmail.com>
2018-09-18 23:38:21 +0100

    GetPageInfo.py updated to return a pagelist of lists containing page number and page text
    Signed-off-by: Adrian <admd911@hotmail.com>

--- GetPageInfo.py
+++ GetPageInfo.py
@@ -1,11 +1,19 @@
-"""Level High"""
-# Go page by page and get page text.
-# For every page, get pageno, any headings, sections, and any footnotes in dict.
-#     Remove the heading, section and footnote tags.
+"""Level 3"""
+
+from OpenPages import get_pages
 
 
-def get_pageinfo(pages):
-    return(pages)
+def get_pageinfo(file, startpage=499, stoppage=712):
+    """Opens the text of the Glosses from the document and returns a page-list of page-no-lists where the page number is
+       at page-no-list[0] and the page text is at page-no-list[1] for a selected range of pages"""
+    pagelist = []
+    for page in range(startpage, stoppage):
+        pageno = page
+        pagetext = get_pages(file, page, page)
+        pagelist.append([pageno, pagetext])
+    return pagelist
 
 
-print(get_pageinfo("hi"))
+# glosses = (get_pageinfo("Wurzburg Glosses", 499, 509))
+# for i in glosses:
+#     print(str(i[0]) + ":\n" + i[1] + "\n")
