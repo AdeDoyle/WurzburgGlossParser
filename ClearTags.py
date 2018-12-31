7fc277d2475006b20d2049a4ae2a823d2fed77b1
Adrian <admd911@hotmail.com>
2018-12-31 00:36:12 +0000

    ClearTags.py updated to detect gloss numbers in latin text using regular expressions, and to account for newer tags now included in the tag-set.
    Signed-off-by: Adrian <admd911@hotmail.com>

--- ClearTags.py
+++ ClearTags.py
@@ -1,14 +1,17 @@
 """Level 1"""
 
 from OpenPages import get_pages
+import re
 
 
 def clear_tags(file, exceptions=[]):
     """Clears all tags out of the text except those listed as exceptions, replacing them with original TPH markers where
        they were originally identified by use of various bracket types"""
     filetext = file
-    taglist = ["H1", "H2", "Lat", "SG", "Eng", "FN", "GLat", "fol", "num", "let", "Rep", "ie", "vel", "Com",
-               "Con", "Sup", "Res", "STOP", "Nam", "MRep", "LRep"]
+    taglist = ["H1", "H2", "Lat", "SG", "Eng", "FN", "GLat", "fol", "num", "let", "Rep", "ie", "vel", "etc", "Com",
+               "Con", "Sup", "Res", "STOP", "Nam", "MRep", "LRep", "...", "&"]
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                          "t", "u", "v", "w", "x", "y", "z"]
     for tag in taglist:
         if tag not in exceptions:
             if tag == "fol":
@@ -23,27 +26,17 @@ def clear_tags(file, exceptions=[]):
                             filetextlist = filetext.split(closetag)
                             filetext = "".join(filetextlist)
             elif tag == "num":
-                for i in range(99):
-                    opentag = "[" + str(i) + "]"
-                    closetag = "[/" + str(i) + "]"
-                    if opentag in filetext:
-                        filetextlist = filetext.split(opentag)
-                        filetext = "".join(filetextlist)
-                    if closetag in filetext:
-                        filetextlist = filetext.split(closetag)
-                        filetext = "".join(filetextlist)
-                    for j in range(99):
-                        opentag = "[" + str(i) + "–" + str(j) + "]"
-                        closetag = "[/" + str(i) + "–" + str(j) + "]"
-                        if opentag in filetext:
-                            filetextlist = filetext.split(opentag)
-                            filetext = "".join(filetextlist)
-                        if closetag in filetext:
-                            filetextlist = filetext.split(closetag)
-                            filetext = "".join(filetextlist)
+                numtagpat = re.compile(r'\[/?\d\w?(–\d\w?)?\]')
+                numtagpatitir = numtagpat.finditer(filetext)
+                numtaglist = []
+                for i in numtagpatitir:
+                    if i.group() not in numtaglist:
+                        numtaglist.append(i.group())
+                for numtag in numtaglist:
+                    filetextlist = filetext.split(numtag)
+                    filetext = "".join(filetextlist)
             elif tag == "let":
-                for l in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-                          "t", "u", "v", "w", "x", "y", "z"]:
+                for l in alphabet:
                     opentag = "[" + l + "]"
                     closetag = "[/" + l + "]"
                     if opentag in filetext:
@@ -93,8 +86,8 @@ def clear_tags(file, exceptions=[]):
 
 def clear_spectags(file, taglist=[]):
     """Clears only the specified tag or tags from the text using the clear_tags function"""
-    alltags = ["H1", "H2", "Lat", "SG", "Eng", "FN", "GLat", "fol", "num", "let", "Rep", "ie", "vel", "Com", "Con",
-               "Sup", "Res", "STOP", "Nam", "MRep", "LRep"]
+    alltags = ["H1", "H2", "Lat", "SG", "Eng", "FN", "GLat", "fol", "num", "let", "Rep", "ie", "vel", "etc", "Com",
+               "Con", "Sup", "Res", "STOP", "Nam", "MRep", "LRep", "...", "&"]
     excepted = []
     for exception in alltags:
         if exception not in taglist:
